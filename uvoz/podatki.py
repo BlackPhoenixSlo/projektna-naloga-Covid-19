c71c431f9ec3abdcb11f2ad1abcfebd3576f3524
Tin Markon <tin.markon@stroka.si>
2022-04-28 10:15:56 +0200

    Testno dodana prva tabela osebe

--- uvoz/podatki.py
+++ uvoz/podatki.py
@@ -5,33 +5,30 @@ import auth
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
-import csv
 
 def ustvari_tabelo():
     cur.execute("""
-        CREATE TABLE obcina (
-            id SERIAL PRIMARY KEY,
+        CREATE TABLE oseba (
+            emso TEXT PRIMARY KEY,
             ime TEXT NOT NULL,
-            povrsina NUMERIC NOT NULL,
-            prebivalstvo INTEGER NOT NULL,
-            gostota NUMERIC NOT NULL,
-            naselja INTEGER NOT NULL,
-            ustanovitev INTEGER,
-            pokrajina TEXT NOT NULL,
-            stat_regija TEXT NOT NULL,
-            odcepitev TEXT
+            priimek TEXT NOT NULL,
+            stalno_prebivalisce TEXT NOT NULL,
+            datum_testiranja DATE NOT NULL,
+            rezultat_testa BOOLEAN NOT NULL
         );
     """)
     conn.commit()
 
 def pobrisi_tabelo():
     cur.execute("""
-        DROP TABLE obcina;
+        DROP TABLE oseba;
     """)
     conn.commit()
 
 conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
-pobrisi_tabelo()
+ustvari_tabelo()
+
+
 
