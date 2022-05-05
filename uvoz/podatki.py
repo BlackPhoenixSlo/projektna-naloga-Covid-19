d7de223fe003c07baa125ca42eb25db2ddcad724
Tin Markon <tin.markon@stroka.si>
2022-05-05 14:21:59 +0200

    Pociscena datoteka za ustvarjanje tabel

--- uvoz/podatki.py
+++ uvoz/podatki.py
@@ -5,40 +5,13 @@ import auth
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
-
-def ustvari_tabelo():
-    cur.execute("""
-        CREATE TABLE oseba (
-            emso TEXT PRIMARY KEY,
-            ime TEXT NOT NULL,
-            priimek TEXT NOT NULL,
-            stalno_prebivalisce TEXT NOT NULL,
-            datum_testiranja DATE NOT NULL,
-            rezultat_testa BOOLEAN NOT NULL
-        );
-    """)
-    conn.commit()
-
-def ustvari_tabelo_cepiva():
-    cur.execute("""
-     CREATE TABLE cepiva (
-            id_cepiva TEXT PRIMARY KEY,
-            ime_cepiva TEXT NOT NULL
-        )
-    """)
-    conn.commit()
-
-def pobrisi_tabelo():
-    cur.execute("""
-        DROP TABLE oseba;
-    """)
-    conn.commit()
-
 conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
 
-ustvari_tabelo_cepiva()
+
+
+
 
 
 
