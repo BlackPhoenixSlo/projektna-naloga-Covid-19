bdfe19ba82745b68b97a7a82b697e1a899ef6fb1
Tin Markon <tin.markon@stroka.si>
2022-05-18 17:00:02 +0200

    Formating

--- uvoz/generiranje_podatkov.py
+++ uvoz/generiranje_podatkov.py
@@ -1,12 +1,14 @@
 ################################################
+from io import StringIO
 from datetime import datetime
 import pandas as pd
 import numpy as np
 from sqlalchemy import null
 import auth
-import psycopg2, psycopg2.extensions, psycopg2.extras
+import psycopg2
+import psycopg2.extensions
+import psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
-from io import StringIO
 
 ################################################
 # Osnovni konfiguracija podatkov
@@ -18,14 +20,15 @@ delez_zensk = 0.511
 julijana_zakrajsek = '28-10-1912'
 verjetnost_testa = 0.15
 # Verjetnost hospitalizacije ob pogoju, da je oseba pozitivna
-verjetnost_hospitalizacije = 0.3 
+verjetnost_hospitalizacije = 0.3
 delez_zdravstvenih_delavcev = 31 / (2 * (10 ** 3))
 
 seed = 1
 rng = np.random.default_rng(seed)
 
-conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+conn = psycopg2.connect(database=auth.db, host=auth.host,
+                        user=auth.user, password=auth.password)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 ################################################
 # Dataframe-i iz katerih bom generiral podatke
@@ -34,12 +37,18 @@ header_names = ["ime", "stevilo"]
 header_surnames = ["priimek", "stevilo"]
 
 # Podatke iz sursa
-moska_imena = pd.read_csv("uvoz/moska_imena.csv", names=header_names, encoding='cp1250', header=0)
-zenska_imena = pd.read_csv("uvoz/zenska_imena.csv", names=header_names, encoding='cp1250', header=0)
-priimki_af = pd.read_csv("uvoz/priimki_af.csv", names=header_surnames, encoding='cp1250', header=0)
-priimki_gl = pd.read_csv("uvoz/priimki_gl.csv", names=header_surnames, encoding='cp1250', header=0)
-priimki_mr = pd.read_csv("uvoz/priimki_mr.csv", names=header_surnames, encoding='cp1250', header=0)
-priimki_sž = pd.read_csv("uvoz/priimki_sž.csv", names=header_surnames, encoding='cp1250', header=0)
+moska_imena = pd.read_csv("uvoz/moska_imena.csv",
+                          names=header_names, encoding='cp1250', header=0)
+zenska_imena = pd.read_csv("uvoz/zenska_imena.csv",
+                           names=header_names, encoding='cp1250', header=0)
+priimki_af = pd.read_csv("uvoz/priimki_af.csv",
+                         names=header_surnames, encoding='cp1250', header=0)
+priimki_gl = pd.read_csv("uvoz/priimki_gl.csv",
+                         names=header_surnames, encoding='cp1250', header=0)
+priimki_mr = pd.read_csv("uvoz/priimki_mr.csv",
+                         names=header_surnames, encoding='cp1250', header=0)
+priimki_sž = pd.read_csv("uvoz/priimki_sž.csv",
+                         names=header_surnames, encoding='cp1250', header=0)
 
 
 # Zdruzevanje priimkov
@@ -57,7 +66,8 @@ priimki["stevilo"] = pd.to_numeric(priimki["stevilo"])
 
 # Izracunavanje deleza kot cenilka za slovensko populacijo
 moska_imena["delez"] = (moska_imena['stevilo'] / moska_imena['stevilo'].sum())
-zenska_imena["delez"] = (zenska_imena['stevilo'] / zenska_imena['stevilo'].sum())
+zenska_imena["delez"] = (zenska_imena['stevilo'] /
+                         zenska_imena['stevilo'].sum())
 priimki["delez"] = (priimki['stevilo'] / priimki['stevilo'].sum())
 
 
@@ -66,11 +76,12 @@ priimki["delez"] = (priimki['stevilo'] / priimki['stevilo'].sum())
 
 def random_date_generator(start_date):
     """Funkcija vraca nakljucni datum, glede na zaceteke. Zadnji datum je danasnji."""
-    num_of_days = (datetime.now() - datetime.strptime(start_date, '%d-%m-%Y')).days
+    num_of_days = (datetime.now() -
+                   datetime.strptime(start_date, '%d-%m-%Y')).days
     days = rng.choice(num_of_days, replace=False)
     random_date = pd.to_datetime(start_date) + pd.DateOffset(days=days)
     str_date = random_date.strftime('%d%m%Y')
-    
+
     return str_date
 
 
@@ -89,7 +100,8 @@ def generiraj_emso(zenska):
 def cepi_osebo():
     # Stevilo 0 uporabljam kot da oseba ni cepljena
     vrednosti = np.arange(0, stevilo_cepiv)
-    verjetnosti_cepiva = [prebivalstvo_slovenije / stevilo_cepiv for _ in range(stevilo_cepiv)]
+    verjetnosti_cepiva = [prebivalstvo_slovenije /
+                          stevilo_cepiv for _ in range(stevilo_cepiv)]
     verjetnosti = [(1 - precepljenost_prebivalstva)] + verjetnosti_cepiva
     cepivo = rng.choice(vrednosti, 1, verjetnosti)[0]
     return cepivo
@@ -98,20 +110,24 @@ def cepi_osebo():
 def testiraj_osebo(verjetnost_testa=verjetnost_testa):
     rezultat_testa = np.random.binomial(1, verjetnost_testa) > 0
     return rezultat_testa
-    
+
 
 def generiraj_osebo(zenska):
     """Funkcija izbere nakljucno ime in nakljucni priimek, nakljucni emso in zgenerira osebo."""
     if zenska:
-        ime = zenska_imena["ime"].sample(n=1, weights=zenska_imena["delez"]).values[0]
-        priimek = priimki["priimek"].sample(n=1, weights=zenska_imena["delez"]).values[0]
+        ime = zenska_imena["ime"].sample(
+            n=1, weights=zenska_imena["delez"]).values[0]
+        priimek = priimki["priimek"].sample(
+            n=1, weights=zenska_imena["delez"]).values[0]
         emso = generiraj_emso(zenska)
-    else :
-        ime = moska_imena["ime"].sample(n=1, weights=moska_imena["delez"]).values[0]
-        priimek = priimki["priimek"].sample(n=1, weights=zenska_imena["delez"]).values[0]
-        emso = generiraj_emso(zenska) 
+    else:
+        ime = moska_imena["ime"].sample(
+            n=1, weights=moska_imena["delez"]).values[0]
+        priimek = priimki["priimek"].sample(
+            n=1, weights=zenska_imena["delez"]).values[0]
+        emso = generiraj_emso(zenska)
     cepljenje = cepi_osebo()
-    return {"ime": ime, 
+    return {"ime": ime,
             "priimek": priimek,
             "emso": emso,
             "stalno_prebivalisce": "Ljubljanska cesta 15",
@@ -139,7 +155,7 @@ def copy_from_stringio(conn, df, table):
     df.to_csv(buffer, index_label='emso', header=False)
     df.to_csv("osebe.csv", index_label='emso', header=False)
     buffer.seek(0)
-    
+
     cursor = conn.cursor()
     try:
         cursor.copy_from(buffer, table, sep=",", null="")
@@ -152,17 +168,20 @@ def copy_from_stringio(conn, df, table):
     print("Prenos na bazo uspešen")
     cursor.close()
 
+
 ###################################################################################
 # 1. tabela - cepiva
-cepivo = pd.Series(["Pfizer-BioNTech", "Moderna", "Johnson & Johnson", "Novavax", "Oxford-AstraZeneca"])
+cepivo = pd.Series(["Pfizer-BioNTech", "Moderna",
+                    "Johnson & Johnson", "Novavax", "Oxford-AstraZeneca"])
 
 
 # 2. tabela - bolnisnice
-ime_bolnisnice = ["Splošna bolnišnica Celje", "Bolnišnica Postojna", "Splošna bolnišnica Izola", "Klinični center Ljubljana", "Splošna bolnišnica Maribor", "Splošna bolnišnica Slovenj Gradec"]
+ime_bolnisnice = ["Splošna bolnišnica Celje", "Bolnišnica Postojna", "Splošna bolnišnica Izola",
+                  "Klinični center Ljubljana", "Splošna bolnišnica Maribor", "Splošna bolnišnica Slovenj Gradec"]
 stevilo_postelj = rng.integers(100, size=len(ime_bolnisnice))
 bolnisnica = pd.DataFrame({"ime_bolnisnice": ime_bolnisnice,
-                            "stevilo_postelj": stevilo_postelj,
-                            })
+                           "stevilo_postelj": stevilo_postelj,
+                           })
 
 
 # Zdravnike razporedim enakomerno po vseh bolnišnicah
@@ -198,20 +217,9 @@ def main():
     #copy_from_stringio(conn=conn, df=cepivo, table="cepivo")
     copy_from_stringio(conn=conn, df=oseba, table="oseba")
     copy_from_stringio(conn=conn, df=pacient, table="pacient")
-    copy_from_stringio(conn=conn, df=zdravstveni_delavec, table="zdravstveni_delavec")
-    
-
+    copy_from_stringio(conn=conn, df=zdravstveni_delavec,
+                       table="zdravstveni_delavec")
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
